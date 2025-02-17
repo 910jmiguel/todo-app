800f8a3c9215fc42b0fe7f94f9d69e264d5f60b5
910jmiguel <910jmiguel@gmail.com>
2025-02-16 22:58:25 -0500

    first commit

---
+++ components/Form.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+type FormProps = {
+  addTodo: (text: string) => void;
+};
+
+const Form = ({ addTodo }: FormProps) => {
+  const [text, setText] = useState("");
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault(); // stops from refreshing
+    if (!text.trim()) {
+      // prevents from adding empty tasks
+      return;
+    }
+    addTodo(text);
+    setText(""); // clears input field after adding
+  };
+
+  return (
+    <form>
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        placeholder="Add new task"
+        className="border p-2"
+      />
+    </form>
+  );
+};
+
+export default Form;
---
+++ types.ts
@@ -0,0 +1,5 @@
+export type Todo = {
+    id: number; // identifier for each to do
+    text: string; // task description
+    completed: boolean; // task done or not (true/false)
+}
